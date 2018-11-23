c0e8efd5de2cc4f86aa16017b81200d862f074d4
shreykpa <shreykpa@DIN73002976.corp.capgemini.com>
2018-11-23 14:37:05 +0530

    Second Attempt

--- GitPractice/src/main/java/com/cap/main/MainClass.java
+++ GitPractice/src/main/java/com/cap/main/MainClass.java
@@ -7,6 +7,11 @@ public class MainClass {
 	public static void main(String[] args) {
 	
 		System.out.println("Hello This is First Try");
+		
+		System.out.println("Hello This is second Try");
+
+		System.out.println("Hello This is third Try");
+
 	}
 
 }
